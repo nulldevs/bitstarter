f3f5a0c2c968a3103234fc12a57dda1dd7a7b4a0
Mateor <mateorod9@gmail.com>
2013-07-10 22:58:52 -0500

    Closer on the whole have a working script issue. Closer.

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var fs = require("fs");
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var servedFile = fs.readFileSync("index.html");
-var content = Buffer.toString(servedFile);
+var servedFile = fs.readFileSync('index.html');
+var buf = new Buffer('servedFile');
 
 app.get('/', function(request, response) {
-  response.send(content);
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
