a2efbbdb0a28c894f700f57750fee05d31f0303e
Mateor <mateorod9@gmail.com>
2013-07-10 23:02:57 -0500

    Working but why not before? hmmmm

--- web.js
+++ web.js
@@ -2,7 +2,6 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-// var servedFile = fs.readFileSync('index.html');
 var buf = new Buffer(fs.readFileSync('index.html')); 
 
 app.get('/', function(request, response) {
